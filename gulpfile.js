b508c18c26f360c3e8cd0ae17c1c57cbe8f787eb
upeoe <upeoe63@163.com>
2017-01-31 18:00:03 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 // 一次性编译 Sass 
-gulp.task('sass', function() {
+gulp.task('build', function() {
     return gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer())
@@ -11,7 +11,7 @@ gulp.task('sass', function() {
 });
 
 // 实时编译
-gulp.task('default', ['sass'], function() {
+gulp.task('watch', ['sass'], function() {
     gulp.watch('./source/scss/_partial/*.scss', ['sass']);
     gulp.watch('./source/scss/*.scss', ['sass']);
 });
